550f219c39f6444020b2cc421ae9760a17a27bd2
delson-uniqnex360 <delson@uniqnex@gmail.com>
2026-04-10 15:39:28 +0530

    added dashboard api

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -2,5 +2,6 @@ from .category import router as category_router
 from .upload import router as upload_router
 from .product import router as product_router
 from .app_import import router as app_import
+from .dashboard import router as dashboard
 
-routers = [category_router, upload_router, product_router, app_import]
+routers = [category_router, upload_router, product_router, app_import, dashboard]
